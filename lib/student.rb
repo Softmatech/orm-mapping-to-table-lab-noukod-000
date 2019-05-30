2b56691224ee1214cb37769fd96795d15855a277
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-30 16:26:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,7 @@ attr_reader :id
 
   def self.create_table
     sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
+      DROP TABLE students
         SQL
     DB[:conn].execute(sql)
   end
